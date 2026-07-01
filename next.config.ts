089457ff6b401e89d4170581149d04d4f820dea3
abeebdon <104466868+abeeebdon@users.noreply.github.com>
2026-07-01 13:57:43 +0100

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'jompstart.us-southeast-1.linodeobjects.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
